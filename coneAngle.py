46471ef77c22e8a942b3b6f49edea6e55bf04a69
issacpv <113247081+issacpv@users.noreply.github.com>
2023-02-24 17:29:00 -0500

    Update coneAngle.py

--- coneAngle.py
+++ coneAngle.py
@@ -3,7 +3,13 @@ import math
 import cv2
 import numpy as np  
 
+i = 0
+
+def nothing(x):
+    pass
+
 def arrayCreator(arr):
+    array = [[0 for x in range(6)] for y in range(0, len(arr))]
     for a in range(0, len(arr)):
         b = arr[i][0]
         m = (b[3]-b[1])/(b[2]-b[0])
@@ -12,28 +18,28 @@ def arrayCreator(arr):
         y1 = ((x1-b[0])*m)+b[1]
         x2 = ((b[2]-b[0])/3)+b[0]+b[0]
         y2 = ((x2-b[0])*m)+b[1]
-        arr[a][x1,y1,x2,y2,m,l]
-        print(arr)
-    return arr
+        array.insert(a, [x1,y1,x2,y2,m,l])
+    return array
 
 def checkLines(arr, arrP):
     arr.sort(key=lambda lst:lst[4])
+    arrBackup = arr
     for a in range(0, len(arr) - 1):
         if arr[a][4] > arr[a+1][4] - 10 and arr[a][4] < arr[a+1][4] + 10:
             if (arr[a][5] > arr[a+1][5]):
                 if(((arr[a][0] - arrP[a][0])*arr[a][4])+arrP[a][1]>arr[a][1]-10 and ((arr[a][0] - arrP[a][0])*arr[a][4])+arrP[a][1] < arr[a][1]+10):
-                    arr = np.delete(arr, a, 0)
+                    arrBackup = np.delete(arrBackup, a, 0)
             else:
                 if(((arr[a+1][0] - arrP[a+1][0])*arr[a+1][4])+arrP[a+1][1]>arr[a+1][1]-10 and ((arr[a+1][0] - arrP[a+1][0])*arr[a+1][4])+arrP[a+1][1] < arr[a+1][1]+10):
-                    arr = np.delete(arr, a+1, 0)
-    return arr
+                    arrBackup = np.delete(arrBackup, a+1, 0)                                                                               
+    return arrBackup
 
 def removeQuad(arr):
     for a in range(0, len(arr) - 1):
         if arr[a][4] > arr[a+1][4] - 10 and arr[a][4] < arr[a+1][4] + 10:
             arr = np.delete(arr, a, 0)
             arr = np.delete(arr, a+1, 0)
-    return arr
+    return arr    
 
 def main(argv):
     default_file = r'C:\Users\vjoji\OneDrive\Pictures\coneMask.png'
@@ -42,22 +48,10 @@ def main(argv):
     dst = cv2.Canny(src, 50, 200, None, 3)
     cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
     cdstP = np.copy(cdst)
-    lines = cv2.HoughLines(dst, 1, np.pi / 180, 10000000, None, 0, 0)
+    lines = cv2.HoughLines(dst, 1, np.pi / 180, 1000000, None, 0, 0)
     
-    if lines is not None:
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-    
-    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 1000000) 
-    linesP = removeQuad(checkLines(arrayCreator(linesP), linesP))
+    linesPr = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 1000000) 
+    linesP = removeQuad(checkLines(arrayCreator(linesPr), linesPr))
 
     if linesP is not None:
         for i in range(0, len(linesP)):
